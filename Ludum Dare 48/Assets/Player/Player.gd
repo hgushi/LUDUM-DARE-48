3ef761aff6140eefd918776138c7f931a05d6bd3
Sid-Joao <miranda.jp2017@gmail.com>
2021-04-24 19:11:50 -0300

    EnemyBase e Player basicos feitos

--- Ludum Dare 48/Assets/Player/Player.gd
+++ Ludum Dare 48/Assets/Player/Player.gd
@@ -1,9 +1,12 @@
 extends KinematicBody2D
+class_name Player
 
 var speed : = Vector2(1000,1000)
 var velocity: = Vector2(0,0)
 var gravity: = 300.0
 var jump_interrupted: = Input.is_action_just_released("jump") and velocity.y < 0
+var health
+
 func _physics_processs(delta):
 	var direction = get_direction()
 	velocity.y += gravity*delta
@@ -13,9 +16,24 @@ func _physics_processs(delta):
 	if jump_interrupted == true:
 		velocity.y = 0.0
 	velocity = move_and_slide(velocity,Vector2(0,-1))
+	if health <= 0:
+		get_tree().paused = true
+		
 
 func get_direction():
 	return Vector2(
 		Input.get_action_strength("move_right") - Input.get_action_strength("move_right"), 
 	- 1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
 	)
+
+func _on_EnemyDetector_area_entered(area):
+	var impulse: = 100
+	velocity.y = -velocity.y*impulse
+
+func _on_EnemyDetector_body_entered(body):
+	take_damage(2)
+	
+func take_damage(amount):
+	health -= amount
+	health = max(health,0)
+	return health
