e0e1d83bc8f59c9d924b1513e207bc3c5f8b98ad
deboraprudencio <debora.prudencio@gmail.com>
2021-04-25 15:27:09 -0300

    Callibrei o player e adicionei as cargas

--- Ludum Dare 48/Levels/Level_D.gd
+++ Ludum Dare 48/Levels/Level_D.gd
@@ -1,10 +1,12 @@
 extends Node2D
 
+var checkpoint = Vector2(0,0)
+
 func _ready():
 	get_node("CanvasLayer").get_node("PauseMenu").visible = false
 	get_node("CanvasLayer").get_node("WinScene").visible = false
 	get_node("CanvasLayer").get_node("LoseScene").visible = false
+	checkpoint = $Player_D.position
 	
 func death():
-	get_node("CanvasLayer/LoseScene").visible = true
-	get_node("CanvasLayer/LoseScene/LoseMusic").play()
+	$Player_D.position = checkpoint
