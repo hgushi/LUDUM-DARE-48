c3be4047aed55f359ba71e5e63b2107aca06d90f
deboraprudencio <debora.prudencio@gmail.com>
2021-04-25 16:12:36 -0300

    Added '9' enemies to level D

--- Ludum Dare 48/Assets/Enemies/9.gd
+++ Ludum Dare 48/Assets/Enemies/9.gd
@@ -1,17 +1,27 @@
 extends KinematicBody2D
 
-var speed : = Vector2(1000,1000)
+var speed = 20
 var velocity: = Vector2(0,0)
-var gravity: = 300.0
+var slide_range = 60
+var distance_walked = 0
+var gravity = 400.0
 
 func _physics_process(delta):
-	velocity.y += gravity*delta
-	if is_on_wall():
-		velocity.x *= -1.0
-	velocity.y = move_and_slide(velocity,Vector2(0,-1)).y
+	velocity.y += gravity * delta
+	if distance_walked < slide_range:
+		if is_on_wall():
+			velocity.x *= -1.0
+			distance_walked += velocity.y
+		else:
+			velocity = move_and_slide(velocity,Vector2(0,0))
+			distance_walked += velocity.x
+		velocity.y = move_and_slide(velocity,Vector2(0,-1)).y
+	else:
+		velocity = - velocity
+		distance_walked = 0
 
 func _ready():
 	set_physics_process(false)
-	velocity.x = - speed.x
+	velocity.x = -speed
 
 
