df63a0976288fdc26d82e90edfe98c84bec0e3ca
Sid-Joao <miranda.jp2017@gmail.com>
2021-04-26 20:10:59 -0300

    Arrumei o 0

--- Ludum Dare 48/Assets/Enemies/0.gd
+++ Ludum Dare 48/Assets/Enemies/0.gd
@@ -13,6 +13,7 @@ onready var PauseTimer = $PauseTimer
 func _ready():
 	velocity.x = -speed.x
 	PauseTimer.start()
+	beam.visible = false
 
 func _physics_process(delta):
 	var box = get_node("Beam").get_node("Area2D").get_node("CollisionShape2D")
@@ -20,7 +21,7 @@ func _physics_process(delta):
 	if is_on_wall():
 		velocity.x *= -1.0
 	velocity.y = move_and_slide(velocity,Vector2(0,-1)).y
-	if AtaqueTimer.is_stopped():
+	if PauseTimer.is_stopped():
 		var player_position = get_parent().get_node("Player").global_position - self.global_position
 		var max_cast_to = player_position.normalized()*max_length
 		RayCast2D.cast_to = max_cast_to
@@ -30,8 +31,9 @@ func _physics_process(delta):
 			end.global_position = RayCast2D.cast_to
 		beam.rotation = RayCast2D.cast_to.angle()
 		beam.region_rect.end.x = end.position.length()
-		
-		box.scale.x = end.position.length()
+		beam.visible = true
+		box.scale.x = end.position.length()/2
+
 func _on_AtaqueTimer_timeout():
 	PauseTimer.start()
 
